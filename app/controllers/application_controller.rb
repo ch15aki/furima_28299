2299a64f8942d61a2e297e6122f636a2d74437c3
ch15aki <and.1la.view@gmail.com>
2020-08-08 23:01:52 +0900

    deviseストロングパラメーターの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # ログインをしていないユーザーはログイン画面に遷移
+  before_action :authenticate_user!
+  # deviseにストロングパラメーターを設定
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+      # deviseのUserモデルにパラメーターを許可
+      devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :first_name, :last_name, :first_name_reading, :last_name_reading, :birthday])
+  end
 end
