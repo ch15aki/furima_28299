ebb48507fa6a8ebc80504cbfa19862f5228edf8a
ch15aki <and.1la.view@gmail.com>
2020-08-09 10:24:42 +0900

    Rubocopを実行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
   def configure_permitted_parameters
-      # deviseのUserモデルにパラメーターを許可
-      devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :first_name, :last_name, :first_name_reading, :last_name_reading, :birthday])
+    # deviseのUserモデルにパラメーターを許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_reading, :last_name_reading, :birthday])
   end
 end
