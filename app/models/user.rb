74984f20c5904a81b174585d162988c0ebeae1a3
ch15aki <and.1la.view@gmail.com>
2020-08-09 10:11:39 +0900

    バリデーション追加
    パスワードを英数字半角のみに設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,7 @@ class User < ApplicationRecord
     validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
     validates :first_name_reading, :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
   end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is valid. Input half-width alphanumeric characters.' 
 end
