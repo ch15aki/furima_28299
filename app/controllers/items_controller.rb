eef11cabad33e59a3f40653d5fdf892adaa54ba5
ch15aki <and.1la.view@gmail.com>
2020-08-10 22:54:10 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,12 +7,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image, :item_name, :description, :price, :shipping_cost_id, :prefecture_id, :shipping_day_id, :condition_id, :category_id).merge(user: current_user.id)
+    params.require(:item).permit(:item_name, :description, :price, :shipping_cost_id, :prefecture_id, :shipping_day_id, :condition_id, :category_id, :image).merge(user_id: current_user.id)
   end
 end
