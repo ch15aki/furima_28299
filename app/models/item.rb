e9f819349432d8f45620b2b37ec772feaed7b9bd
ch15aki <and.1la.view@gmail.com>
2020-08-10 15:59:19 +0900

    ActiveHashモデルのアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,11 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefecture, :shipping_cost, :shipping_days, :condition, :category
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_cost
+  belongs_to_active_hash :shipping_day
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :category
+
   belongs_to :user
 
   has_one_attached :image
