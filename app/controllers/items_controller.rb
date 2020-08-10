647735547345a4240c56e2c232b32bddf2709b39
ch15aki <and.1la.view@gmail.com>
2020-08-10 14:18:18 +0900

    createアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,14 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :description, :price, :shipping_cost_id, :prefecture_id, :shipping_days_id, :condition_id, :category_id).merge(user: current_user.id)
+  end
 end
