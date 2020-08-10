90bd6fc42d3516b9b005c04b62ad98ac5c3cf53b
ch15aki <and.1la.view@gmail.com>
2020-08-10 15:35:00 +0900

    shipping_daysモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,15 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefecture, :shipping_cost
+  belongs_to_active_hash :prefecture, :shipping_cost, :shipping_days
   belongs_to :user
 
   has_one_attached :image
 
   with_options presence: true do
-    validates :prefecture, :shipping_cost
+    validates :prefecture, :shipping_cost, :shipping_days
   end
 
   with_options numericality: { other_than: 1 } do
-    validates :prefecture_id, :shipping_cost_id
+    validates :prefecture_id, :shipping_cost_id, :shipping_days_id
   end
 end
