0331ece2d61591efd620318b03cb91defc788659
ch15aki <and.1la.view@gmail.com>
2020-08-10 15:49:57 +0900

    categoryモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,15 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefecture, :shipping_cost, :shipping_days, :condition
+  belongs_to_active_hash :prefecture, :shipping_cost, :shipping_days, :condition, :category
   belongs_to :user
 
   has_one_attached :image
 
   with_options presence: true do
-    validates :prefecture, :shipping_cost, :shipping_days, :condition
+    validates :prefecture, :shipping_cost, :shipping_days, :condition, :category
   end
 
   with_options numericality: { other_than: 1 } do
-    validates :prefecture_id, :shipping_cost_id, :shipping_days_id, :condition_id
+    validates :prefecture_id, :shipping_cost_id, :shipping_days_id, :condition_id, :category_id
   end
 end
