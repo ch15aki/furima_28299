bb7445aee21972cd83f57cce26998dfe340fbb3f
ch15aki <and.1la.view@gmail.com>
2020-08-10 15:19:06 +0900

    prefectureモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
   belongs_to :user
 
   has_one_attached :image
+
+  with_options presence: true do
+    validates :prefecture
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :prefecture_id
+  end
 end
