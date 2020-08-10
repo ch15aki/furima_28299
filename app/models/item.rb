5d3501a53dad05f2ae3cff9b129f116d7b74ea0b
ch15aki <and.1la.view@gmail.com>
2020-08-10 15:27:34 +0900

    shipping_costモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,15 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :prefecture, :shipping_cost
   belongs_to :user
 
   has_one_attached :image
 
   with_options presence: true do
-    validates :prefecture
+    validates :prefecture, :shipping_cost
   end
 
   with_options numericality: { other_than: 1 } do
-    validates :prefecture_id
+    validates :prefecture_id, :shipping_cost_id
   end
 end
