1fd12b331480476098652af8fddb6647e7ec8b13
ch15aki <and.1la.view@gmail.com>
2020-08-11 22:20:32 +0900

    テスト用itemモデル作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,13 @@
 FactoryBot.define do
   factory :item do
+    item_name {Faker::String.random}
+    description {Faker::Lorem.sentence}
+    price {Faker::Number.within(range: 300..9999999)}
+    shipping_cost_id {Faker::Number.within(range: 1..2)}
+    prefecture_id {Faker::Number.within(range: 1..47)}
+    shipping_day_id {Faker::Number.within(range: 1..3)}
+    condition_id {Faker::Number.within(range: 1..6)}
+    category_id {Faker::Number.within(range: 1..10)}
+    association :user
   end
 end
